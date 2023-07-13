2f9b3c1cf5af413eadc769ceea8c52117f9db5f0
AlexBecci <beccialex2002@gmail.com>
2023-07-13 11:51:12 -0300

    test for object task

--- tests/index.test.js
+++ tests/index.test.js
@@ -16,17 +16,25 @@ describe("GET /tasks", () => {
 });
 
 describe("POST /tasks", () => {
-  //Should respons with a 200 status code
-  test("shouuld respond with a 200 status code", async () => {
-    const response = await request(app).post("/tasks").send();
-    expect(response.statusCode).toBe(200);
-  });
-  //should respond with a content-type of application/json
-  test("should have a content-type: application.json in header", async () => {
-    const response = await request(app).post("/tasks").send();
-    expect(response.headers["content-type"]).toEqual(
-      expect.stringContaining("json")
-    );
-  });
+    //Should respons with a 200 status code
+    test("shouuld respond with a 200 status code", async () => {
+      const response = await request(app).post("/tasks").send();
+      expect(response.statusCode).toBe(200);
+    });
+    //should respond with a content-type of application/json
+    test("should have a content-type: application.json in header", async () => {
+      const response = await request(app).post("/tasks").send();
+      expect(response.headers["content-type"]).toEqual(
+        expect.stringContaining("json")
+      );
+    });
   //should respond with a json object containing the new task with an id
+  test("should respond with an task ID", async () => {
+    const response = await request(app).post("/tasks").send({
+      title: "test taks",
+      description: "test Description",
+    });
+    expect(response.body.id).toBeDefined();
+    console.log(response.body.id);
+  });
 });
