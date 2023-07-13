1468405098ceaa51eb1e5a9d9d4f97881db8287c
AlexBecci <beccialex2002@gmail.com>
2023-07-13 11:34:23 -0300

    other test method POST

--- tests/index.test.js
+++ tests/index.test.js
@@ -8,4 +8,20 @@ describe("GET /tasks", () => {
     const response = await request(app).get("/tasks").send();
     expect(response.statusCode).toBe(200);
   });
+
+  test("should respond with an array", async () => {
+    const response = await request(app).get("/tasks").send();
+    expect(response.body).toBeInstanceOf(Array);
+  });
+});
+
+describe("POST /tasks", () => {
+  //Should respons with a 200 status code
+  test("shouuld respond with a 200 status code", async () => {
+    const response = await request(app).post("/tasks").send();
+    expect(response.statusCode).toBe(200);
+  });
+  //should respond with a content-type of application/json
+
+  //should respond with a json object containing the new task with an id
 });
